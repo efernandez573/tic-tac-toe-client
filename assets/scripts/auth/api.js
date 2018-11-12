654d59faf19e708c24cc5545ae9eb4f28d469368
efernandez573 <elsiefernandez@gmail.com>
2018-11-11 23:03:53 -0500

    updated game logic and added change password AJAX request also added clear form with hidden formatting

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -2,42 +2,65 @@
 const config = require('../config.js')
 const store = require('../store.js')
 
-const signUp = data => {
+const signUp = function (data) {
   return $.ajax({
-    url: config.apiUrl + '/sign-up',
+    url: config.apiUrls + '/sign-up',
     method: 'POST',
     data
   })
 }
-const signIn = data => {
+const signIn = function (data) {
   return $.ajax({
-    url: config.apiUrl + '/sign-in',
+    url: config.apiUrls + '/sign-in',
     method: 'POST',
     data
   })
 }
-const signOut = data => {
+const signOut = function (data) {
   return $.ajax({
-    url: config.apiUrl + '/sign-out',
+    url: config.apiUrls + '/sign-out',
     method: 'DELETE',
     headers: {
       Authorization: 'Token token=' + store.user.token
-    }
+    },
+    data
   })
 }
-const createGame = function () {
+const changePassword = function (data) {
   return $.ajax({
-    url: config.apiUrl + '/games',
+    url: config.apiUrls + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+const createGame = function (data) {
+  return $.ajax({
+    url: config.apiUrls + '/games',
     method: 'POST',
     headers: {
       Authorization: 'Token token=' + store.user.token
     },
-    data: {}
+    data
   })
 }
+const Stats = function () {
+  return $.ajax({
+    url: config.apiUrl + `/games/${store.game.id}`,
+    method: 'GET',
+    headers: {
+      Authorization: `Token token=${store.user.token}`
+    }
+  })
+}
+
 module.exports = {
   signUp,
   signIn,
   signOut,
-  createGame
+  changePassword,
+  createGame,
+  Stats
 }
